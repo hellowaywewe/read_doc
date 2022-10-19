9fa1640ff734532965b3304c4218996d245b9c8d
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-19 20:18:19 +0000

    Adding tarfile member sanitization to extractall()

--- tinyms/data/utils.py
+++ tinyms/data/utils.py
@@ -63,7 +63,26 @@ def _unzip(gzip_path):
     # decompress the file if it ends with `.tar`
     if gzip_file.endswith('.tar'):
         with tarfile.open(gzip_file) as f:
-            f.extractall(gzip_file[:gzip_file.rfind('/')])
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(f, gzip_file[:gzip_file.rfind("/")])
 
 
 def _fetch_and_unzip(url, file_name):
